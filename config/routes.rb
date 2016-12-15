efc7c83d5bc32c5f8fa19c769a234c5d4de4410d
roccia <lzzy.ywz@gmail.com>
2016-12-15 21:52:10 +0800

    button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   get 'auto_car/index'
   root 'auto_car#index'
-  post 'auto_car/move' => 'auto_car#move'
+  get 'auto_car/move' => 'auto_car#move'
+  get 'auto_car/stop' => 'auto_car#stop'
+  get 'auto_car/back' => 'auto_car#back'
+  get 'auto_car/speed' => 'auto_car#speed'
+  get 'auto_car/left' => 'auto_car#left'
+  get 'auto_car/right' => 'auto_car#right'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
